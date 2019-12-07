47467246744be60e4325b55f938e228275e2d8fc
ESSARRAJ <essarraj.fouad@gmail.com>
2019-12-07 15:42:04 +0100

    Ajoutez des fichiers projet.

---
+++ TraineeManagementApp.Win/Data/AppModel.cs
@@ -0,0 +1,19 @@
+﻿using AppGestionStagiaires.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestionStagiaires.Data
+{
+    public class AppModel : DbContext
+    {
+        public AppModel() : base(@"Data Source=.\SQLEXPRESS;Initial Catalog=TP_TraineeManagement_EF;user = sa; password = admintp4")
+        {
+
+        }
+        public virtual DbSet<Group> Groups { get; set; }
+    }
+}
---
+++ TraineeManagementApp.Win/Services/GroupService.cs
@@ -0,0 +1,65 @@
+﻿using AppGestionStagiaires.Data;
+using AppGestionStagiaires.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppGestionStagiaires.Services
+{
+    /// <summary>
+    /// Les données sont enregistrer dans une liste des groupes
+    /// La minipulation des données est gérer avec Linq
+    /// </summary>
+    public class GroupsService
+    {
+        private AppModel _Context; 
+
+        public GroupsService()
+        {
+            _Context = new AppModel();
+        }
+        /// <summary>
+        /// Add new Group
+        /// </summary>
+        /// <param name="g"> le Groupe à ajouter</param>
+        public void Add(Group g) {
+            this._Context.Groups.Add(g);
+            this._Context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Supprimer un groupe par Id
+        /// </summary>
+        /// <param name="id"> Id de groupe à supprimer</param>
+        public void Delete(int id) {
+            var db_group = this._Context.Groups.Find(id);
+            if (db_group != null)
+            {
+                _Context.Groups.Remove(db_group);
+                _Context.SaveChanges();
+            }
+    
+        }
+
+        /// <summary>
+        /// Mise à jour un objet Groupe
+        /// </summary>
+        /// <param name="g">un objet de type groupe contenant les information à mettre à jour</param>
+        public void Update(Group g) {
+            var db_group = this._Context.Groups.Find(g.Id);
+            if (db_group != null)
+            {
+                db_group.Nom = g.Nom;
+                _Context.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Retourne la liste des groupes à afficher
+        /// </summary>
+        /// <returns></returns>
+        public List<Group> FindAll() {
+           return  _Context.Groups.ToList();
+        }
+    }
+}
